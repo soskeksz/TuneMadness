9abc57d95c668df405af3de38f5415d55189c81a
[Zoltai Barna] <you@example.com>
2023-07-01 21:53:38 +0200

    add some server code

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,39 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const EmployeeModel = require("./db/employee.model");
+const EquipmentModel = require("./db/equipment.model");
+
+const { MONGO_URL, PORT = 8080 } = process.env;
+
+if (!MONGO_URL) {
+  console.error("Missing MONGO_URL environment variable");
+  process.exit(1);
+}
+
+const app = express();
+app.use(express.json());
+
+
+//ENDPOINTS
+
+
+
+
+
+
+
+const main = async () => {
+    await mongoose.connect(MONGO_URL);
+  
+    app.listen(PORT, () => {
+      console.log("App is listening on 8080");
+      console.log("Try /api/employees route right now");
+    });
+  };
+  
+  main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+  
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Layout from "./Pages/Layout";
+import ErrorPage from "./Pages/ErrorPage";
+import EmployeeList from "./Pages/EmployeeList";
+import EmployeeCreator from "./Pages/EmployeeCreator";
+import EmployeeUpdater from "./Pages/EmployeeUpdater";
+
+import "./index.css";
+import TableTest from "./Pages/TableTest";
+import FormTest from "./Pages/FormTest";
+import EquipmentList from "./Pages/EquipmentList";
+import EquipmentCreator from "./Pages/EquipmentCreator";
+import EquipmentUpdater from "./Pages/EquipmentUpdater";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <EmployeeList />,
+      },
+      {
+        path: "/employees/:search",
+        element: <EmployeeList />,
+      },
+      {
+        path: "/create",
+        element: <EmployeeCreator />,
+      },
+      {
+        path: "/update/:id",
+        element: <EmployeeUpdater />,
+      },
+      {
+        path: "/table-test",
+        element: <TableTest />,
+      },
+      {
+        path: "/form-test",
+        element: <FormTest />,
+      },
+      {
+        path: "/equipments",
+        element: <EquipmentList/>
+      },
+      {
+        path: "/create-equipment",
+        element: <EquipmentCreator/>
+      },
+      {
+        path: "/equipment-update/:id",
+        element: <EquipmentUpdater/>
+      }
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
